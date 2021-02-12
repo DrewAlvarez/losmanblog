c2c96e179f3316166e3c3ef877af0230048a4646
Drew A <alvarez.drew@gmail.com>
2021-02-11 18:51:36 -0600

    get basic react started

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <h2>Welcome to LosMan Reviews</h2>
+      </div>
+      <p className="App-intro">
+        Check out reviews on Horror Movies old and new, domestic an foreign!
+      </p>
+    </div>
+  );
+}
+
+
+export default App;
