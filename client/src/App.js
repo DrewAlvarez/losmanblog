9b04ff8b83da75a2cade4a885ff8c3d055116488
Drew A <alvarez.drew@gmail.com>
2021-02-12 19:59:46 -0600

    bootstrap, navbar, font added

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,20 @@
 import React, { Component } from "react";
+import Navbar from "./components/Navbar"
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <div className="App-header">
-        <h2>Welcome to LosMan Reviews</h2>
+    <>
+      <Navbar />
+      <div className="App">
+        <div className="App-header">
+          <h2>Welcome to LosMan Reviews</h2>
+        </div>
+        <p className="App-intro">
+          Check out reviews on Horror Movies old and new, domestic an foreign!
+        </p>
       </div>
-      <p className="App-intro">
-        Check out reviews on Horror Movies old and new, domestic an foreign!
-      </p>
-    </div>
+    </>
   );
 }
 
